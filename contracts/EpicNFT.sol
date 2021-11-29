98dfbcd3848b8982662a900a8bc9721b944db994
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2021-11-29 17:56:16 +0800

    generate random words in svg

--- contracts/EpicNFT.sol
+++ contracts/EpicNFT.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "hardhat/console.sol";
 
+
+import { Base64 } from "./libraries/Base64.sol";
 
 contract EpicNFT is ERC721URIStorage {
 	// Magic given to us by OpenZeppelin to help us keep track of tokenIds.
@@ -12,7 +15,7 @@ contract EpicNFT is ERC721URIStorage {
   Counters.Counter private _tokenIds;
 
 
-	string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+	string public baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
 
 	string[] public firstWords = ["Faye", "Valentine", "Cowboy", "Bebop", "Spike", "Jet"];
@@ -28,9 +31,28 @@ contract EpicNFT is ERC721URIStorage {
 	function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
 		uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
 
-		rand = rand & firstWords.length;
+		rand = rand % firstWords.length;
+    console.log(rand);
 		return firstWords[rand];
 	}
+
+	function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+    rand = rand % secondWords.length;
+    console.log(rand);
+    return secondWords[rand];
+  }
+
+  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+    rand = rand % thirdWords.length;
+    console.log(rand);
+    return thirdWords[rand];
+  }
+
+	function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
+  }
 
 
   // A function our user will hit to get their NFT.
@@ -38,6 +60,40 @@ contract EpicNFT is ERC721URIStorage {
 		
 		// Get the current tokenId, this starts at 0.
 		uint256 newItemId = _tokenIds.current();
+
+
+		string memory first = pickRandomFirstWord(newItemId);
+    string memory second = pickRandomSecondWord(newItemId);
+    string memory third = pickRandomThirdWord(newItemId);
+    string memory combinedWord = string(abi.encodePacked(first, second, third));
+
+		string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+		
+
+		// Get all the JSON metadata in place and base64 encode it.
+    string memory json = Base64.encode(
+        bytes(
+            string(
+                abi.encodePacked(
+                    '{"name": "',
+                    combinedWord,
+                    '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                    Base64.encode(bytes(finalSvg)),
+                    '"}'
+                )
+            )
+        )
+    );
+
+		string memory finalTokenUri = string(
+        abi.encodePacked("data:application/json;base64,", json)
+    );
+
+
+		console.log("\n--------------------");
+    console.log(finalTokenUri);
+    console.log("--------------------\n");
 
 		// Actually mint the NFT to the sender using msg.sender.
 		_safeMint(msg.sender, newItemId);
@@ -47,7 +103,7 @@ contract EpicNFT is ERC721URIStorage {
 
 
 		// Set the NFTs data.
-		_setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSbUZxYVhSaGMxRjFaWE5oWkdsc2JHRnpRMmhwYldsamFHRnVaMkZ6UEM5MFpYaDBQZ284TDNOMlp6ND0iCn0=");
+		_setTokenURI(newItemId, finalTokenUri);
 		console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
 		// Increment the counter for when the next NFT is minted.
